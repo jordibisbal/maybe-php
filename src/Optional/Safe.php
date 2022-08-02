e3781d66bcfb306b4ceaa1d10e6af1f90f637950
Jordi Bisbal <jordi.bisbal@gmail.com>
2022-08-02 06:14:55 +0200

    Added anyway mainly to easy debugging on optional usage Changed assertions, so it works with a wider range of phpunit version

--- src/Optional/Safe.php
+++ src/Optional/Safe.php
@@ -8,7 +8,7 @@ namespace j45l\maybe\Optional;
  * @template C
  * @phpstan-param (callable(mixed=, mixed=, mixed=, mixed=, mixed=, mixed=, mixed=, mixed=, mixed=, mixed=):C) $function
  * @param mixed $parameters
- * @phpstan-return Optional<C>
+ * @phpstan-return Optional<mixed>
  */
 function safe(callable $function, ...$parameters): Optional
 {
